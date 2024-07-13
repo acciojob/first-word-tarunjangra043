a899a09ee176984a2d42de1c462dded0ab406011
Tarun Jangra <111531292+tarunjangra043@users.noreply.github.com>
2024-07-13 16:15:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function firstWord(s) {
   // your code here
+	let words = s.split(' ');
+	return words[0];
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+// const s = prompt("Enter String:");
 alert(firstWord(s));
